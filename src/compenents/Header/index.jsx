20ac43bcaafedbe69a05477e9720c625bdfacd29
LuthGom <lucianomendes23@yahoo.com>
2022-08-09 16:16:59 -0300

    Header: implemetand responsividade do componente Header

--- src/compenents/Header/index.jsx
+++ src/compenents/Header/index.jsx
@@ -7,7 +7,7 @@ import { Link } from "react-router-dom";
 
 function Home() {
   return (
-    <div className={styles.container}>
+    <header className={styles.container}>
       <div className={styles.categorias}>
         <Categorias />
       </div>
@@ -16,17 +16,17 @@ function Home() {
           <img
             src={logo1}
             alt="Logo tipo da loja com imagem de tubarão com fundo branco e arc-íris"
-            width="150px"
+         
           />
           <img
             src={logo2}
             alt="Logo tipo da loja com imagem de tubarão com fundo preto e arc-íris"
-            width="150px"
+           
           />
         </Link>
       </div>
       <NavBar />
-    </div>
+    </header>
   );
 }
 
