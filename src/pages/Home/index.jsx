a848410b9695d1d7e913385bfc29905e2aa50814
LuthGom <lucianomendes23@yahoo.com>
2022-08-11 14:55:07 -0300

    Home: retirando div que encapsulava Banner

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -2,9 +2,9 @@ import Banner from "../../components/Banner";
 import styles from "./Home.module.css";
 function Home() {
   return (
-    <div className={styles.banner}>
+    
       <Banner />
-    </div>
+    
   );
 }
 
