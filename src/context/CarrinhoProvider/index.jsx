94e262bf762a95a33a10ba7a311158b101abee4c
LuthGom <lucianomendes23@yahoo.com>
2022-08-12 20:14:08 -0300

    CarrinhoProvider: state qtde para contar quantidade de cada produto do carrinho no modal

--- src/context/CarrinhoProvider/index.jsx
+++ src/context/CarrinhoProvider/index.jsx
@@ -4,19 +4,34 @@ export const CarrinhoContext = createContext();
 
 export default function CarrinhoProvider({ children }) {
   const [lista, setLista] = useState([]);
-  const [total, setTotal] = useState([]);
+  const [qtde, setQtde] = useState([]);
+  console.log(total);
   function addItemAoCarrinho(item) {
-    var incl = lista.includes(item);
-    if (!incl) {
+    const novoItem = lista.includes(item);
+    if (!novoItem) {
       setLista([...lista, item]);
+      setQtde([...qtde, item])
       console.log(lista);
     } else {
       setLista(lista);
     }
   }
+  function incrementaProdutos(produto) {
+    setQtde([...qtde, produto]);
+  }
+  function decrementaOuRemove(produto) {
+    setLista(qtde.splice(qtde.indexOf(produto), 1));
+  }
   return (
     <CarrinhoContext.Provider
-      value={{ lista, setLista, total, setTotal, addItemAoCarrinho }}
+      value={{
+        lista,
+        setLista,
+        addItemAoCarrinho,
+        incrementaProdutos,
+        decrementaOuRemove,
+        qtde,
+      }}
     >
       {children}
     </CarrinhoContext.Provider>
