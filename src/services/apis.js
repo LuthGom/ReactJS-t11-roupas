d33868bfaf5715acb8098765bf4d12daf16beb18
LuthGom <lucianomendes23@yahoo.com>
2022-08-15 22:12:12 -0300

    apis: add url de Login

--- src/services/apis.js
+++ src/services/apis.js
@@ -1,4 +1,8 @@
+import axios from "axios";
+
 export const produtosUrl = (categoria) =>
   `https://api-loja-t11.herokuapp.com/produtos${categoria}`;
 
-
+export const LoginURL = axios.create({
+  baseURL: "https://restaurant-client-api.herokuapp.com/cliente/",
+});
