e80cfa5b7912d676d6bd816719943fb92162d881
LuthGom <lucianomendes23@yahoo.com>
2022-08-17 19:52:47 -0300

    NavBar: add lógica para botão de Login sumir da NavBar quando um usuário tiver logado

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -1,9 +1,15 @@
 import styles from "./NavBar.module.css";
 import { Link } from "react-router-dom";
+import { useAuth } from "../../context/AuthProvider/useAuth";
 function NavBar() {
+  const auth = useAuth();
   return (
     <div className={styles.container}>
-      <Link to="/login">Login/Cadastro</Link>
+      {(auth.email && (
+        <Link style={{ display: "none" }} to="/login">
+          Login/Cadastro
+        </Link>
+      )) || <Link to="/login">Login/Cadastro</Link>}
       <Link to="/contato">Contato</Link>
       <Link to="/sobre">Sobre</Link>
     </div>
