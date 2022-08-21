1df355475e69092f51d58b2980ec869177b76953
LuthGom <lucianomendes23@yahoo.com>
2022-08-21 00:25:08 -0300

    CarrinhoModal: tirando toda a lógica de carrinho e passando para o novo hok customizad: useCarrinho

--- src/components/CarrinhoModal/index.jsx
+++ src/components/CarrinhoModal/index.jsx
@@ -1,21 +1,17 @@
-import { useContext } from "react";
 import Modal from "react-modal";
-import { CarrinhoContext } from "../../context/CarrinhoProvider";
-import BotaoCarrinho from "../BotaoCarrinho";
+import useCarrinho from "../../hooks/useCarrinho/useCarrinho";
 import styles from "./Modal.module.css";
-import { Link } from "react-router-dom"
+
 Modal.setAppElement("#root");
 
 export default function CarrinhoModal({
   isOpen,
   onRequestClose,
   contentLabel,
-  onClick,
-  botaoNome,
+onClick
 }) {
-  const { lista, incrementaProdutos, decrementaOuRemove, qtde } =
-    useContext(CarrinhoContext);
 
+  
   return (
     <Modal
       overlayClassName={styles.overlayContainer}
@@ -25,64 +21,7 @@ export default function CarrinhoModal({
       onAfterOpen={styles.contentContainer}
       className={styles.containerModal}
     >
-      <div className={styles.containerListaCards}>
-        <div>
-          {lista.map((produto, index) => {
-            return (
-              <div className={styles.containerCard} key={index}>
-                <img src={produto.url_imagem} alt={produto.descricao} />
-                <p className={styles.titulo}>{produto.titulo}</p>
-                <p className={styles.preco}> R$ {produto.preco}</p>
-                <p className={styles.descricao}>{produto.descricao}</p>
-                <div className={styles.counter}>
-                  <BotaoCarrinho
-                    addOuRemove="+"
-                    click={() => incrementaProdutos(produto)}
-                  ></BotaoCarrinho>
-                  <p>{qtde.filter((item) => item === produto).length}</p>
-                  <BotaoCarrinho
-                    addOuRemove="-"
-                    click={() => {
-                      decrementaOuRemove(produto);
-                      if (
-                        qtde.filter((item) => item === produto).length === 0
-                      ) {
-                        decrementaOuRemove(produto);
-                      }
-                    }}
-                  ></BotaoCarrinho>
-                </div>
-              </div>
-            );
-          })}
-        </div>
-      </div>
-      <div className={styles.modalFooter}>
-        <button className={styles.closeButton} onClick={onClick}>
-          {botaoNome}
-        </button>
-        <span>
-          Total: R$
-          {(qtde.length > 0 &&
-            qtde
-              .map((produto) => {
-                let qtdeDoProduto = qtde.filter(
-                  (item) => item === produto
-                ).length;
-                if ((qtdeDoProduto = 1)) {
-                  return produto.preco;
-                }
-                return qtdeDoProduto * produto.preco;
-              })
-              .reduce((atual, acum) => {
-                return atual + acum;
-              })) ||
-            "0,00"}
-        </span>
-        <div className={styles.finalizarCompra}>
-          <Link to="/carrinho" onClick={onClick}>Finalizar Compra</Link>
-        </div>
-      </div>
+      {useCarrinho("X", onClick)}
     </Modal>
   );
 }
