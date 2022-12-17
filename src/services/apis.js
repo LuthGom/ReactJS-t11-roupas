5676e0b2dbe325ecb612ff04cfd011c8514d609a
LuthGom <lucianomendes23@yahoo.com>
2022-12-17 18:38:22 -0300

    Fix: trocando url para listagem de produtos

--- src/services/apis.js
+++ src/services/apis.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const produtosUrl = (categoria) =>
-  `https://api-loja-t11.herokuapp.com/produtos${categoria}`;
+  `https://api-de-compras.onrender.com/produtos${categoria}`;
 
 export const LoginURL = axios.create({
   baseURL: "https://restaurant-client-api.herokuapp.com/cliente/",
