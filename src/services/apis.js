1631e80ad661f041346f863880bad383178bfb12
LuthGom <lucianomendes23@yahoo.com>
2022-12-17 23:56:59 -0300

    dev: alterando url de login

--- src/services/apis.js
+++ src/services/apis.js
@@ -4,5 +4,5 @@ export const produtosUrl = (categoria) =>
   `https://api-de-compras.onrender.com/produtos${categoria}`;
 
 export const LoginURL = axios.create({
-  baseURL: "https://restaurant-client-api.herokuapp.com/cliente/",
+  baseURL: "https://login-clients-api.onrender.com/cliente/",
 });
